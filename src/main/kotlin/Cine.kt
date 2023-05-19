1067cfb569d98fe763c56c2f4ea452a70515177e
Diego Gutierrez <dieguga04@gmail.com>
2023-05-19 20:38:05 +0200

    cine creado

---
+++ src/main/kotlin/Cine.kt
@@ -0,0 +1,29 @@
+open class Cine(nFilas:Int,nAsientos:Int,nombreCine:String = "Yelmo Plus"){
+
+    val numero_filas = 10
+    val numero_asientos =  15
+    val nombre_cine = nombreCine
+
+
+    open fun mostrarCine(){
+
+    }
+
+    open fun venderEntrada(){
+
+    }
+
+    open fun devolverEntrada(){
+
+    }
+
+    open fun leerVentas_actualizarCine(){
+
+    }
+
+    open fun guardarventas(){
+
+    }
+
+
+}
