d05a6cff5b8abdd0dc7700dc49e317d68c0568e6
Diego Gutierrez <dieguga04@gmail.com>
2023-05-19 20:50:13 +0200

    arreglo de errores

--- src/main/kotlin/Cine.kt
+++ src/main/kotlin/Cine.kt
@@ -1,27 +1,27 @@
-open class Cine(nFilas:Int,nAsientos:Int,nombreCine:String = "Yelmo Plus"){
+class Cine(nFilas:Int,nAsientos:Int,nombreCine:String = "Yelmo Plus"){
 
     val numero_filas = 10
     val numero_asientos =  15
     val nombre_cine = nombreCine
 
 
-    open fun mostrarCine(){
+     fun mostrarCine(){
 
     }
 
-    open fun venderEntrada(){
+     fun venderEntrada(){
 
     }
 
-    open fun devolverEntrada(){
+     fun devolverEntrada(){
 
     }
 
-    open fun leerVentas_actualizarCine(){
+     fun leerVentas_actualizarCine(){
 
     }
 
-    open fun guardarventas(){
+     fun guardarventas(){
 
     }
 
